a0125a63b2493efa6e75963b5ddd85a9e881711c
Mattia <scaccia.mattia@gmail.com>
2021-01-05 15:20:46 +0100

    Sistemato dataset, 5 cluster totali: user, journal, special-issue, research, conference

--- spider-prova.py
+++ spider-prova.py
@@ -1,12 +1,16 @@
 import scrapy
+from scrapy.spiders import CrawlSpider,Rule
+from scrapy.linkextractors import LinkExtractor
 
-
-class MySpider(scrapy.Spider):
+class MySpider(CrawlSpider):
     name = 'guide2research.com'
     allowed_domains = ['guide2research.com']
-
-    def start_requests(self):
-        yield scrapy.Request('https://www.guide2research.com', self.parse)
+    start_urls = ['https://www.guide2research.com']
+    base_url = 'https://www.guide2research.com'
+    
+    rules = [Rule(LinkExtractor(allow = ["/u/","/journal/","/conference/","/special-issue/","/research/"]), callback='parse', follow=False), 
+             Rule(LinkExtractor(allow =('')))]
+    
 
     def parse(self, response):
         filename = response.url + '.html'
@@ -14,11 +18,11 @@ class MySpider(scrapy.Spider):
         filename = filename.replace('https://www.guide2research.com/', '')
         filename = filename.replace('https://guide2research.com/', '')
         filename = filename.replace('/','?')
-        filename = './guide2research/' + filename
+        filename = './prova/' + filename
         with open(filename, 'wb') as f:
             f.write(response.body)
-        for href in response.xpath('//a/@href').getall():
-            yield scrapy.Request(response.urljoin(href), self.parse)
+        #for href in response.xpath('//a/@href').getall():
+        #    yield scrapy.Request(response.urljoin(href), self.parse)
 
 
 
