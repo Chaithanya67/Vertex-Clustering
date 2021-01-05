6f98dd0679c56c30576717850e75361e5be89f6b
Antonio Turco <truock@gmail.com>
2021-01-05 20:39:45 +0100

    :sparkles: :construction: adds a first draft for the creation of the shingle hashed vector

--- core/ShingleExtractor.py
+++ core/ShingleExtractor.py
@@ -31,3 +31,43 @@ def extract_shingle_set(web_page, window_size):
 		index += 1
 
 	return shingle
+
+#probabilmente merita di andare in un altro file, forse ShingleVectorFactory
+import numpy as np # per uint8, da valutare
+import mmh3
+from functools import partial
+
+def get_hash_functions(k):
+	hash = lambda seed, value: mmh3.hash(value, seed)
+	hashes = [ partial(hash, i) for i in range(0,k)] 
+	return hashes
+
+def min_hash(shingle_set, hash_function):
+	flatten_shingle = lambda shingle: [tag for window in shingle for tag in window]
+	shingle_set = flatten_shingle(shingle_set)
+	shingle_hashed = list(
+		map(
+			lambda tag: hash_function(tag) % 256, # da verificare
+			shingle_set
+		)
+	)
+	return min(shingle_hashed)
+
+def create_shingle_vector(shingle_set):
+	k_hash = get_hash_functions(8)
+	shingle_vector = []
+	for hash_function in k_hash:
+		shingle_byte = min_hash(shingle_set, hash_function)
+		shingle_vector.append(shingle_byte)
+	return shingle_vector
+
+#example
+s = create_shingle_vector([
+			["html", "head"], 
+			["head", "head"],
+			["head", "body"],
+			["body", "body"],
+			["body", "html"]
+])
+
+print(s)
