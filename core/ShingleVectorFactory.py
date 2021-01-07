7bdfd625df5cd078846c0b04b65533c1ba7681e3
Alexandru Pavel <alexandru.pavel.94@gmail.com>
2021-01-07 23:48:59 +0100

    hashes shingles correctly

--- core/ShingleVectorFactory.py
+++ core/ShingleVectorFactory.py
@@ -3,26 +3,29 @@ import mmh3
 from functools import partial
 from core.shingleVector import ShingleVector
 
+
 def get_hash_functions(k):
-	hash = lambda seed, value: mmh3.hash(value, seed)
-	hashes = [ partial(hash, i) for i in range(0,k)] 
-	return hashes
+    hash = lambda seed, value: mmh3.hash(value, seed)
+    hashes = [partial(hash, i) for i in range(0, k)]
+    return hashes
+
 
 def min_hash(shingle_set, hash_function):
-	flatten_shingle = lambda shingles: [tag for shingle in shingles for window in shingle.getContent() for tag in window]
-	shingle_set = flatten_shingle(shingle_set)
-	shingle_hashed = list(
-		map(
-			lambda tag: hash_function(tag) % 256, # da verificare
-			shingle_set
-		)
-	)
-	return min(shingle_hashed)
+    hashed_shingle_set = list(
+        map(
+            # converte le liste di tag in una stringa unica con separatore, poi ne calcola l'hash
+            lambda shingle: hash_function(','.join(shingle.getContent())) % 256, # da verificare hashing
+            shingle_set
+        )
+    )
+    # restituisce l'hash minimo tra tutti quelli degli shingle della pagina
+    return min(hashed_shingle_set)
+
 
 def create_shingle_vector(shingle_set):
-	k_hash = get_hash_functions(8)
-	shingle_vector = []
-	for hash_function in k_hash:
-		shingle_byte = min_hash(shingle_set, hash_function)
-		shingle_vector.append(shingle_byte)
-	return ShingleVector(shingle_set[0],shingle_vector)
+    k_hash = get_hash_functions(8)
+    shingle_vector = []
+    for hash_function in k_hash:
+        shingle_byte = min_hash(shingle_set, hash_function)
+        shingle_vector.append(shingle_byte)
+    return ShingleVector(shingle_set[0], shingle_vector)
